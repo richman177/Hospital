f910f5fbdc94884dfaae0a03ac398af3d3bc1d55
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-06-13 17:42:09 +0600

    Update models.py
    dgfdgfd

--- mysite/hospital_app/models.py
+++ mysite/hospital_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
-from multiselectfield import MultiSelectField
+from multiselectfield import MultiSelectField      
 
 ROLE_CHOICES = (
     ('doctor', 'doctor'),

43d2d159d2977a3099a6691e96cfa140f6bdf0ce
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-08 10:05:16 +0600

    Update models.py
    s

--- mysite/hospital_app/models.py
+++ mysite/hospital_app/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models 
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
